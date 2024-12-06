7136832a8635a1c5ef5f8cd54c15383e3cef68f2
Nahom Tsegaye <tnahom214@gmail.com>
2024-12-06 15:25:09 +0300

    try-1.9

--- src/app/(auth)/gymForm/page.tsx
+++ src/app/(auth)/gymForm/page.tsx
@@ -1,6 +1,10 @@
 
 "use client"
+
 export const dynamic = "force-dynamic";
+export const runtime = "nodejs"; // Add this line
+
+
 import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
