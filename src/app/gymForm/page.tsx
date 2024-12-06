8e45853f5356cda4fe3d7d9dbce92f6c44071af9
Nahom Tsegaye <tnahom214@gmail.com>
2024-12-07 00:21:42 +0300

    add dynamic force to sign in and not found file

--- src/app/gymForm/page.tsx
+++ src/app/gymForm/page.tsx
@@ -5,8 +5,9 @@ import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import Link from "next/link"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from 'react-hook-form';
+
 import { z } from "zod"
 import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
 import { gymFormSchema, signUpFormSchema } from "@/lib/auth-schema"
