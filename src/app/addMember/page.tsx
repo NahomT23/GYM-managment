8488fb40a33a0ecf184d1714ffaa9acf4ed6fa0a
Nahom Tsegaye <tnahom214@gmail.com>
2024-12-10 20:46:30 +0300

    try-3.7

--- src/app/addMember/page.tsx
+++ src/app/addMember/page.tsx
@@ -249,14 +249,14 @@ const AddMember = async () => {
             placeholder="Age"
           />
         </div>
-        <Link href={dashboardLink}>
+
         <button
           type="submit"
           className="w-full bg-blue-500 text-white rounded-lg px-4 py-2 mt-4 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400"
         >
           Add Member
         </button>
-        </Link>
+
         <div className="flex items-center justify-center mt-4">
         <Link href={dashboardLink}>
           <Button>Back to Dashboard</Button>
